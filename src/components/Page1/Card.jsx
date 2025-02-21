cbec53a05faf017f6a4abc1c2e93d46186953ff3
Deepak <ds262969@gmail.com>
2025-02-21 22:16:24 +0530

    Made the moving div text animation effect

--- src/components/Page1/Card.jsx
+++ src/components/Page1/Card.jsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Card = () => {
+  const tiltRef = useRef(null);
+  const [xVal, setXVal] = useState(0);
+  const [yVal, setYVal] = useState(0);
+
+  const mouseMoving = (e) => {
+    setXVal(
+      (e.clientX -
+        tiltRef.current.getBoundingClientRect().x -
+        tiltRef.current.getBoundingClientRect().width / 2) /
+        10
+    );
+    setYVal(
+      -(
+        e.clientY -
+        tiltRef.current.getBoundingClientRect().y -
+        tiltRef.current.getBoundingClientRect().height / 2
+      ) / 10
+    );
+    tiltRef.current.style.transform = `rotateX(${yVal}deg) rotateY(${xVal}deg)`;
+  };
+
+  const mouseLeave = () => {
+    tiltRef.current.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  };
+
   return (
     <div
       style={{
         backgroundImage: "url(/images/ANZO.jpg)",
+        perspective: "1000px",
       }}
       className="w-full h-[95vh] rounded-[40px] p-15 bg-cover bg-center bg-no-repeat relative overflow-hidden"
     >
       {/* <img className="w-full h-full" src="/images/ANZO.jpg" /> */}
-      <div className="flex justify-between items-center">
+      <div className="relative flex justify-between items-center">
         <img src="/images/logo.png" className="w-18 drop-shadow-xl" />
         <div className="flex gap-3 items-center">
           <div className="bg-white px-0.5 py-0.5 rounded-full">
-            <button className="bg-black px-5 py-1 rounded-full text-white">
+            <button className="bg-black px-5 py-1 rounded-full text-white cursor-pointer">
               Hire Me
             </button>
           </div>
           <BsThreeDotsVertical color="#fff" size={"1.2em"} />
         </div>
       </div>
-      <div className="mt-30 w-[45%]">
+      <div
+        ref={tiltRef}
+        onMouseMove={(e) => mouseMoving(e)}
+        onMouseLeave={() => mouseLeave()}
+        className="mt-30 w-[45%] transition-all"
+      >
         <h1 className="text-7xl text-white font-anzo-bold">
           I AM{" "}
           <span className="text-black">
@@ -32,7 +63,7 @@ const Card = () => {
         </h1>
       </div>
       <div className="mt-30 w-[45%]">
-        <h4 className="text-white font-anzo-regular">
+        <h4 className="text-white text-xl font-anzo-regular">
           BRAND DESIGN | WEBSITE DESIGN
         </h4>
       </div>
